c407ade6105d64b6b2f97c44ce51f5734d8c39c5
root <sonya12slyvia@gmail.com>
2024-03-16 09:03:01 -0700

    correction number 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(f"{chr(i)}", end="")
-    else:
-        print(f"{chr(i - 32)}", end="")
+for i in range(122, 96, -2):
+    print("{}".format(chr(i)), end="")
+    print("{}".format(chr(i - 33)), end="")
