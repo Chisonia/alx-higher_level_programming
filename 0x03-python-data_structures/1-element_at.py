2d4bf0735a9cf2fecc92619b9f001d2fd2bb7ced
root <sonya12slyvia@gmail.com>
2024-03-18 14:53:22 -0700

    corrected number 1 again again again

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if my_list[idx] < 0:
         return None
-    elif my_list[idx] > len(my_list):
+    elif my_list[idx] > len(my_list) - 1:
         return None
     else:
         return my_list.pop(idx)
