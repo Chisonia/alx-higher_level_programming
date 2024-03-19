30b77e8f972e14e4f5147f9e729073ce068f35d8
root <sonya12slyvia@gmail.com>
2024-03-19 15:26:40 -0700

    number 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{:d} arguments.".format(x))
+    elif x == 1:
+        print("{:d} argument:".format(x))
+    else:
+        print("{:d} arguments:".format(x))
+    for i in range(x):
+        print("{:d}: {:s}".format(i + 1, argv[i + 1]))
