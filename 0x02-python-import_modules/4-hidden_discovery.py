83251f4abe1c5a2802838c572f171524c170bac2
root <sonya12slyvia@gmail.com>
2024-03-19 16:07:43 -0700

    corrected 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     function_names = dir()
-    for fun in range(len(function_names)):
-        if function_names[fun][:2] != "__":
-            print("{:s}".format(function_name[fun]))
+    for i in range(0, len(function_names)):
+        if function_names[i][:2] != "__":
+            print("{:s}".format(function_name[i]))
