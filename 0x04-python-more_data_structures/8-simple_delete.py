d4cc82adb6c79dc8d673683ab41c4134808faafc
root <sonya12slyvia@gmail.com>
2024-03-23 06:30:57 -0700

    correct 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
+    if a_dictionary:
         try:
             del a_dictionary[key]
         except KeyError:
             pass
         return a_dictionary
+
 
