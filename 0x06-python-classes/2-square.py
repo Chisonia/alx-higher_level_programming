be5f9c9ff3660ed1b05ece2ac00cd2af7a6de69d
root <sonya12slyvia@gmail.com>
2024-04-15 05:14:34 -0700

    number 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''This is Square module'''
+
+
+class Square:
+    '''This represents a  square'''
+
+    def __init__(self, size=0):
+        '''initialiazing an instance of the square class
+        with a default value for size'''
+        if not isinstance(size, int):
+            '''checking if the value passed for size is an integer'''
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            '''checking if value passed for size is a positive number'''
+            raise ValueError("size must be >= 0")
+        self.__size = size
