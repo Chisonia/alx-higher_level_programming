c07f2f2ba3851c82cc7ca5a349509994ecb463ac
root <sonya12slyvia@gmail.com>
2024-04-15 04:21:00 -0700

    added comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+'''Square module'''
+
 
 class Square:
+    '''This is a Square'''
 
     def __init__(self, size):
+        '''This is an instance of the square class'''
 
         self.__size = size
+        '''self.__size is an object variale'''
