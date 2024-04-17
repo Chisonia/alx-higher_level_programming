d14505afb9dbe68841ddb86c7bd3f4de5f4d85b0
root <sonya12slyvia@gmail.com>
2024-04-17 12:53:14 -0700

    correct

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,6 +75,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        '''Compares 2 rectangle'''
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
