221a9f0c58dad74a73c1ab407e76f48ed9f93abc
root <sonya12slyvia@gmail.com>
2024-05-22 04:37:05 -0700

    Updated create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Create a table if it doesn't exist
 -- with id and name attribute
-USE hbtn_0c_0; CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
