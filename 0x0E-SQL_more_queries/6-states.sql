79b3aeade2f73264c84285026de9317469c287eb
chisonia <sonya12slyvia@gmail.com>
2024-05-24 16:10:22 +0100

    number 6 update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,7 +6,8 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.unique_id(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
     id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL ,
     name VARCHAR NOT NULL(256)
 );
