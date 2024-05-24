8d2fc40e377419e1314e9ec8e5a68c5160895a38
chisonia <sonya12slyvia@gmail.com>
2024-05-24 17:07:45 +0100

    Up 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,9 +7,12 @@
 -- The database name will be passed as an argument of the mysql command
 mysql -u root -p < hbtn_0d_tvshows.sql
 
-USE hbtn_0d_tvshows;
-
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+  FROM tv_shows
+  INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+  WHERE EXISTS (
+    SELECT 1
+    FROM tv_show_genres AS g2
+    WHERE g2.tv_show_id = tv_shows.id
+  )
+  ORDER BY tv_shows.title, tv_show_genres.genre_id;
