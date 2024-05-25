9f320fbf38e5467c0624dedbd91e129c7fa7f3a2
chisonia <sonya12slyvia@gmail.com>
2024-05-25 03:34:52 +0100

    Update 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,8 +5,8 @@
 -- If a show doesn’t have a genre, display NULL
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-mysql -u root -p < 10-genre_id_by_show.sql
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
