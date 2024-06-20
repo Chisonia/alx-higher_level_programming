7f56787b14222ca5ed9207e11e6406e0f64aadaa
chisonia <sonya12slyvia@gmail.com>
2024-06-20 17:54:58 +0100

    corrected

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 let i;
-const x = process.argv[2]? parseInt(process.argv[2]) : undefined;
+const x = parseInt(process.argv[2]);
 
-if (x === undefined) {
-    console.log('Missing number of occurrences');
-} else if (isNaN(x)) {
-    console.log('Invalid number of occurrences');
-} else if (x < 0) {
-    return 1;
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < x; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
