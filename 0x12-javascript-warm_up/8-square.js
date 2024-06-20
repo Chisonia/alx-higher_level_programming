ccc094250932c09b7a6d2afe1e70f29519d8a3c9
chisonia <sonya12slyvia@gmail.com>
2024-06-20 18:05:06 +0100

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let i;
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
