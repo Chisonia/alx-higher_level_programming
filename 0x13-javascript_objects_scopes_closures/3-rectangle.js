72e45c8026e7acdc5997e9ee70cafabf21d1c819
chisonia <sonya12slyvia@gmail.com>
2024-06-21 02:46:42 +0100

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      console.log('Empty object');
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
