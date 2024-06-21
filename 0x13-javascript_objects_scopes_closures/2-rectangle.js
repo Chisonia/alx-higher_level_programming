557e623d2b9a6615f6ec6465d154ccc8257dd7d6
chisonia <sonya12slyvia@gmail.com>
2024-06-21 01:43:37 +0100

    correct

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 && typeof w === 'number' && typeof h === 'number') {
+    if ((typeof w === 'number' && w > 0 && Number.isInteger(w)) && (typeof h === 'number' && h > 0 && Number.isInteger(h))) {
       this.width = w;
       this.height = h;
     } else {
