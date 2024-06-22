e9d36ad4e258081608bb8ae34ffbaa542ecfe91e
chisonia <sonya12slyvia@gmail.com>
2024-06-22 05:26:43 +0100

    c

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,12 +3,10 @@
 class Rectangle {
   constructor (w, h) {
     if (w === undefined || h === undefined || w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
-      this.width = w;
-      this.height = h;
+      return {}; // Return empty object if invalid parameters are provided
     }
+    this.width = w;
+    this.height = h;
   }
 }
 
