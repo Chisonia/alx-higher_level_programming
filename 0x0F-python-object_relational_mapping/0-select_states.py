8dad1aabc457984cc023aa7e30ef646a16095be0
chisonia <sonya12slyvia@gmail.com>
2024-06-27 17:35:35 +0100

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 
+if __name__ == '__main__':
+    import sys
+    import MySQLdb 
 
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],  # Use 'passwd' instead of 'password'
+        db=sys.argv[3]  # Use 'db' instead of 'db_name'
+    )
+
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+
+    cursor = db.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    for item in result:
+        print(item)
+    
+    cursor.close()
+    db.close()
